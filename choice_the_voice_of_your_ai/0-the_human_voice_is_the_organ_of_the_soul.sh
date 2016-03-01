4db05eb82ef0c4d92516dfd81629a1d18c068e22
dora <dora.korpar@gmail.com>
2016-03-01 09:12:14 -0800

    finished from 3 days ago

--- choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
+++ choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-message=$1;
-address=$3;
+message=$1
+address=$3
 case $2 in
-    "f") voice=Anges
+    "f") voice=Agnes
 	;;
     "m") voice=Albert
 	;;
-    "x") voice=Bad News
+    "x") voice="Bad News"
 	;;
 esac
 
-filename=echo $message | awk '{print $1}'
-say -v $voice -o $filename.m4a $message
+filename=$(echo $message | awk '{print $1}')
+say -v $voice -o $filename.m4a $1
 
-scp $filename.m4a admin@$address
+scp $filename.m4a admin@$address:/var/www/IOF.com/meower/
+
+echo "Listen to the message on http://$address/$filename.m4a"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
